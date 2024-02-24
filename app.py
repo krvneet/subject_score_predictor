5658a9a0a0610a3203bd71d2707c1d2c590010a1
Vineet <krvinay9250@gmail.com>
2024-02-24 13:39:10 +0530

    deploy app streamlit

--- app.py
+++ app.py
@@ -1,71 +1,59 @@
 import numpy as np
 import pickle
 import pandas as pd
-#from flasgger import Swagger
 import streamlit as st 
-
 from PIL import Image
 
-#app=Flask(__name__)
-#Swagger(app)
+preprocessor = pickle.load(open("artifacts\proprocessor.pkl","rb"))
+classifier=pickle.load(open("artifacts\model.pkl","rb"))
 
-pickle_in = open("artifacts\model.pkl","rb")
-classifier=pickle.load(pickle_in)
-
-pickle_in2 = open("artifacts\proprocessor.pkl","rb")
-preprocessor = pickle.load(pickle_in2)
-
-
-
-
-
-#@app.route('/')
 def welcome():
     return "Welcome All"
 
-#@app.route('/predict',methods=["Get"])
-def predict_note_authentication(gender,race,parent,lunch,test,read,write):
-    d = {
-        'gender': gender,
-        'race_ethnicity': race,
-        'parental_level_of_education': parent,
-        'lunch': lunch,
-        'test_preparation_course': test,
-        'reading_score': read,
-        'writing_score': write
-    }
-    l = pd.DataFrame(d,index=[0])
-    x = preprocessor.fit_transform(l)
-    prediction=classifier.predict(x)
+def predict_note_authentication(df):
+    x = preprocessor.transform(df)
+    prediction=classifier.predict(x)[0]
     print(prediction)
     return prediction
 
-
-
 def main():
-    st.title("Bank Authenticator")
+    st.title("Application")
     html_temp = """
     <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
+    <h2 style="color:white;text-align:center;">Student Score Predictor</h2>
     </div>
     """
     st.markdown(html_temp,unsafe_allow_html=True)
-    gender = st.text_input("gender","Type Here")
-    race = st.text_input("race","Type Here")
-    parent = st.text_input("parent","Type Here")
-    lunch = st.text_input("lunch","Type Here")
-    test= st.text_input("test","Type Here")
-    read = st.text_input("read","Type Here")
-    write = st.text_input("write","Type Here")
+    
+    # Main Code
+    input_text = []
+    for i in range(5):
+        input_text.append(st.text_input("Enter Text Input {}:".format(i+1)))
+    
+    input_num = []
+    for i in range(2):
+        input_num.append(st.number_input("Enter Number Input {}:".format(i+1)))
+
+    d = {
+        'gender': input_text[0],
+        'race_ethnicity': input_text[1],
+        'parental_level_of_education': input_text[2],
+        'lunch': input_text[3],
+        'test_preparation_course': input_text[4],
+        'reading_score': input_num[0],
+        'writing_score': input_num[1]
+    }
+
+    df = pd.DataFrame(d, index=[0])
 
     result=""
+
     if st.button("Predict"):
-        result=predict_note_authentication(gender,race,parent,lunch,test,read,write)
-    st.success('The output is {}'.format(result))
-    if st.button("About"):
-        st.text("Lets LEarn")
-        st.text("Built with Streamlit")
+        try:
+            result=predict_note_authentication(df)
+            st.success(f'The output is {result:.2f}')
+        except Exception as e:
+            print(f"maa chud gyi hai error: {e}")
 
 if __name__=='__main__':
     main()
-    
